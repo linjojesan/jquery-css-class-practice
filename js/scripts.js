ed061b9b37172af7c4663f219aa54d7ac46b4ab6
Bobby Martin and John Linneman <john@>
2018-03-20 09:46:39 -0700

    Add jQuery and css files. Apply jQuery to body color theme.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$().ready(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+
+  });
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+
+
+
+  });
+});
